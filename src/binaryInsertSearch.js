cb82ef8f904519e96563e446a4824a2e907faf81
YuTengjing <2713151713@qq.com>
2019-04-10 17:57:40 +0800

    Refactor: binaryInsertSearch speed

--- src/binaryInsertSearch.js
+++ src/binaryInsertSearch.js
@@ -3,40 +3,20 @@ const binaryInsertSearch = (array, value, startIndex = 0, endIndex = array.lengt
     if (array.length === 0) return 0;
     if (array.length === 1) return value < array[0] ? 0 : 1;
 
-    if (startIndex === endIndex) {
-        if (value < array[startIndex]) {
-            return startIndex;
-        } else {
-            let j = startIndex + 1
-            for (; j < array.length; j++) {
-                if (value < array[j]) return j;
-            }
-
-            if (j === array.length) {
-                return array.length;
-            }
-        }
-    } else if (startIndex > endIndex) {
-        return startIndex;
+    if (startIndex >= endIndex) {
+        return value >= array[startIndex] ? startIndex + 1 : startIndex;
     }
 
     const middleIndex = (startIndex + endIndex) / 2 | 0;
 
-    if (value > array[middleIndex]) {
-        startIndex = middleIndex + 1;
-        return binaryInsertSearch(array, value, startIndex, endIndex);
-    } else if (value < array[middleIndex]) {
+    if (value < array[middleIndex]) {
         endIndex = middleIndex - 1;
         return binaryInsertSearch(array, value, startIndex, endIndex);
-    } else if (value === array[middleIndex]) {
-        let j = middleIndex + 1;
-        for (; j < array.length; j++) {
-            if (value < array[j]) return j;
-        }
-
-        if (j === array.length) {
-            return array.length;
-        }
+    } else if (value > array[middleIndex]) {
+        startIndex = middleIndex + 1;
+        return binaryInsertSearch(array, value, startIndex, endIndex);
+    } else {
+        return middleIndex + 1;
     }
 }
 
--- test/binaryInsertSearch.js
+++ test/binaryInsertSearch.js
@@ -35,10 +35,10 @@ describe('#binaryInsertSearch', () => {
     })
 
     it('#should binaryInsertSearch([4, 4, 4, 4], 4) return 4', () => {
-        assert.equal(binaryInsertSearch([4, 4, 4, 4], 4), 4);
+        assert.equal(binaryInsertSearch([4, 4, 4, 4], 4), 2);
     })
 
     it('#should binaryInsertSearch([4, 4, 4, 4, 4], 4) return 5', () => {
-        assert.equal(binaryInsertSearch([4, 4, 4, 4, 4], 4), 5);
+        assert.equal(binaryInsertSearch([4, 4, 4, 4, 4], 4), 3);
     })
 })
