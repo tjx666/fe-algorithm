04df99d7057146301d311eb4f840b55befc31f71
YuTengjing <ytj2713151713@gmail.com>
2020-02-24 17:13:40 +0800

    :sparkles: feat: add solution and tests for addTwoLinkList

---
+++ src/algorithm/linkList/addTwoList.ts
@@ -0,0 +1,24 @@
+import LinkList from '../../dataStructure/linkList/linkList';
+
+/**
+ * 9 -> 1 -> 2
+ * 1 -> 2 -> 3
+ * =>
+ * 0 -> 4 -> 5
+ */
+export default function addTwoList(list1: LinkList, list2: LinkList): LinkList {
+    const list3 = new LinkList();
+    let carry = 0;
+    let current1 = list1.head;
+    let current2 = list2.head;
+
+    while (current1 != null || current2 != null) {
+        const sum = (current1?.value || 0) + (current2?.value || 0) + carry;
+        carry = sum >= 10 ? 1 : 0;
+        list3.append(sum % 10);
+        current1 = current1?.next;
+        current2 = current2?.next;
+    }
+
+    return list3;
+}
---
+++ test/algorithm/linkList/addTwoList.test.ts
@@ -0,0 +1,20 @@
+import assert = require('assert');
+import addTwoList from '../../../src/algorithm/linkList/addTwoList';
+import LinkList from '../../../src/dataStructure/linkList/linkList';
+
+describe('#addTwoList', () => {
+    it('should return the list which is addTwoList', () => {
+        const list1 = new LinkList();
+        list1
+            .append(9)
+            .append(2)
+            .append(3);
+        const list2 = new LinkList();
+        list2
+            .append(1)
+            .append(2)
+            .append(3);
+        const list3 = addTwoList(list1, list2);
+        assert.strictEqual(list3.toString(), '0 -> 5 -> 6');
+    });
+});
